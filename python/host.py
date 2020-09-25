e1a977477e1a463c86e64e3c95afac7f5674d01d
amit kaushik <atkaushik@gmail.com>
2020-09-25 13:57:20 +0530

    python

---
+++ python/host.py
@@ -0,0 +1,11 @@
+import socket
+def host_IP():
+   try:
+      hname = socket.gethostname()
+      hip = socket.gethostbyname(hname)
+      print("Hostname:  ",hname)
+      print("IP Address: ",hip)
+   except:
+      print("Unable to get Hostname and IP")
+# Driver code
+host_IP() #Function call
